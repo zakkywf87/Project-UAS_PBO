dc6ec4cd6c6845c8101ee1816cc6d20c7fdfd411
ASUS <ASUS@LAPTOP-N9T5KQDD>
2024-06-15 14:09:01 +0700

    first commit

---
+++ src/main/java/com/mycompany/applikasipemantaugizi06/Applikasipemantaugizi06.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.applikasipemantaugizi06;
+
+/**
+ *
+ * @author Asus
+ */
+public class Applikasipemantaugizi06 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
